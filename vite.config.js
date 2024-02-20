6ea85e8abbb45f6b165a804eef8ffc4eb2d9bd7a
Vaibhaw Mishra <vaibhavmishra658@gmail.com>
2024-02-20 13:13:27 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:{
+  build: {
     outDir: "build",
-    rollupOptions:{
-      external:[
-        "react-icons/Gi"
-      ]
-    }
-  }
-})
+    rollupOptions: {
+      external: ["react-icons/Gi", "react-icons/Lu", "react-icons/Bi"],
+    },
+  },
+});
